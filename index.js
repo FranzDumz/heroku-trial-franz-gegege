9a19528408ec0e2eb15bbeeb632ef4b5cc68a186
unknown <gdslink.francisdumo@gmail.com>
2020-12-04 16:48:56 +0800

    trial API

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+// import required essentials
+const http = require('http');
+const express = require('express');
+var cors = require('cors');
+// import `items` from `routes` folder 
+const itemsRouter = require('./routes/items');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+// create new app
+const app = express();
+app.use(express.json());
+// use it before all route definitions
+// allowing below URL to access these APIs end-points
+// you can replace this URL(http://localhost:8100) with your
+// application URL from where you are calling these APIs
+app.use(cors({origin: 'http://localhost:5432'}));
+
+/* this '/items' URL will have two end-points:
+→ localhost:3000/items/ (this returns array of objects)
+→ localhost:3000/items/:id (this returns single object)
+*/
+app.use('/items', itemsRouter);
+
+// default URL to API
+app.use('/', function(req, res) {
+    res.send('node-ex-api works :-)asdsdasda');
+});
+
+const server = http.createServer(app);
+const port = 5432;
+server.listen(port);
+console.debug('Server listening on port ' + port);
