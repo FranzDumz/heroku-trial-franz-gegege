ad1622bda9aee913c27e56abff91b74cc77f925c
unknown <gdslink.francisdumo@gmail.com>
2020-12-04 17:59:58 +0800

    process enc port

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ app.use('/', function(req, res) {
 
 const server = http.createServer(app);
 const port = 5432;
-server.listen(port);
+server.listen(process.env.PORT || 5000);+
 console.debug('Server listening on port ' + port);
