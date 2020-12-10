b41c023ed593381afd2b1fac26ecc126b31196dd
unknown <gdslink.francisdumo@gmail.com>
2020-12-10 17:10:11 +0800

    database trial

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-// import required essentials
-const http = require('http');
-const express = require('express');
-var cors = require('cors');
-// import `items` from `routes` folder 
-const itemsRouter = require('./routes/items');
-
-// create new app
-const app = express();
-app.use(express.json());
-// use it before all route definitions
-// allowing below URL to access these APIs end-points
-// you can replace this URL(http://localhost:8100) with your
-// application URL from where you are calling these APIs
-app.use(cors({origin: 'https://obscure-tor-85795.herokuapp.com/'}));
-
-/* this '/items' URL will have two end-points:
-→ localhost:3000/items/ (this returns array of objects)
-→ localhost:3000/items/:id (this returns single object)
-*/
-app.use('/items', itemsRouter);
-
-// default URL to API
-app.use('/', function(req, res) {
-    res.send('node-ex-api works :-)sadadddasdasdas');
-});
-
-const server = http.createServer(app);
-const port = 5432;
-server.listen(process.env.PORT || 5432);+
-console.debug('Server listening on port ' + port);
+const pg = require('pg')
+const config = {
+    database: 'dejn8c6mlm6ov2'
+}
+const pool = new pg.Pool(config);
+pool.connect((err, client, done) => {
+    if (err) throw err;
+    client.query('SELECT  * FROM  info', (err, res) => {
+        if (err)
+            console.log(err.stack);
+        else {
+            console.log(res.rows);
+        }
+        pool.end()
+    })
+})
