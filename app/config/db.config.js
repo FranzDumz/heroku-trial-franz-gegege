892cbe57465d77d9db135934982399b4d452310e
unknown <gdslink.francisdumo@gmail.com>
2020-12-10 18:10:26 +0800

    database trial

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "ec2-54-147-126-202.compute-1.amazonaws.com",
+    USER: "hathabrfnfutvw",
+    PASSWORD: "1dc84f6b60653ce9246dbd03b15c71d233711f5ef82bebd0508528e359b6dc03",
+    DB: "dejn8c6mlm6ov2",
+    dialect: "postgres",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
+  }
