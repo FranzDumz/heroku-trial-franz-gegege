a203a6ebc0e4d8ce0aae7139b77cd1cc3090a703
unknown <gdslink.francisdumo@gmail.com>
2020-12-10 18:48:48 +0800

    asdasda

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 
 // set port, listen for requests
 const PORT = process.env.PORT || 5432;
-require("../app/routes/turorial.routes.js")(app);
+// require("../app/routes/turorial.routes.js")(app);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
