0c7c24f9810ac56ef4b308631488bd9183656075
unknown <gdslink.francisdumo@gmail.com>
2020-12-11 11:48:59 +0800

    directory fixed

--- server.js
+++ server.js
@@ -19,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to heroku application." });
 });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 5432;
- require("../app/routes/turorial.routes.js")(app);
+ require("./routes/turorial.routes.js")(app);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
